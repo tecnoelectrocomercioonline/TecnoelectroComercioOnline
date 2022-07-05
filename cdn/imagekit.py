15ce7d82e57325948ffa5ec50abfea5c8002e3a9
nigarumovum <bfpr131095@gmail.com>
2022-07-05 09:22:42 -0600

    modelfix

--- cdn/imagekit.py
+++ cdn/imagekit.py
@@ -1,16 +1,14 @@
- from imagekitio import ImageKit
+from imagekitio import ImageKit
 imagekit = ImageKit(
-    private_key='your private_key',
-    public_key='your public_key',
-    url_endpoint='your url_endpoint'
+    private_key='private_LoyP/7BRnXZKci25YcGyUqV3Xkw=',
+    public_key='public_ZYAWyTf3Z1mDntD50znHaHd5Y5Y=',
+    url_endpoint='https://ik.imagekit.io/tecnoelectrocr'
 )
-
 # 1. Using Image path and image hostname or endpoint
-
-
 imagekit_url = imagekit.url({
     "path": "/default-image.jpg",
             "url_endpoint": "https://ik.imagekit.io/your_imagekit_id/endpoint/",
             "transformation": [{"height": "300", "width": "400"}],
 }
 )
+
