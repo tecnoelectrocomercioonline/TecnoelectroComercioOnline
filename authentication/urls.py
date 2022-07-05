d1640bd8e94b2ef717b19600fe298711f6f1379d
nigarumovum <bfpr131095@gmail.com>
2022-07-05 11:35:55 -0600

    urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from django.conf import settings
+from authentication import views as user_view
 from . import views
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login_user/', user_view.login_user, name='login_user'),
+    path('logout_user/', auth.LogoutView.as_view(template_name='shop/body.html'),
+         name='logout_user'),
+    path('register/', user_view.register, name='register'),
+    path('activate-user/<uidb64>/<token>',
+         user_view.activate_user, name='activate'),
 ]
--- ecom/urls.py
+++ ecom/urls.py
@@ -8,13 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
     path("i18n/", include("django.conf.urls.i18n")),
     path('', include('shop.urls')),
+    path('', include('authentication.urls')),
     path('', include('api.urls')),
-    path('login_user/', user_view.login_user, name='login_user'),
-    path('logout_user/', auth.LogoutView.as_view(template_name='shop/body.html'),
-         name='logout_user'),
-    path('register/', user_view.register, name='register'),
-    path('activate-user/<uidb64>/<token>',
-         user_view.activate_user, name='activate'),
 ]
     
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
