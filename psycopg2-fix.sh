8d85fa68fed64d7d59ae912125fc6c74d205118d
nigarumovum <bfpr131095@gmail.com>
2022-07-08 12:05:54 -0600

    123123

---
+++ psycopg2-fix.sh
@@ -0,0 +1,7 @@
+brew install libpq --build-from-source
+brew install openssl
+
+export LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib -L/opt/homebrew/opt/libpq/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include -I/opt/homebrew/opt/libpq/include"
+
+pip3 install psycopg2
