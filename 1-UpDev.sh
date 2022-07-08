722288cac1fa3b2e32072c64b1da8093a45f6304
nigarumovum <bfpr131095@gmail.com>
2022-07-08 11:57:23 -0600

    reqs

--- 1-UpDev.sh
+++ 1-UpDev.sh
@@ -1,8 +1,17 @@
 # Fix install errors
+pip install -U pip
+pip3 install -U pip
 python3 -m pip install --upgrade pip
 python3 -m pip3 install --upgrade pip
 pip3 install --upgrade setuptools
+pip install ez_setup
 pip3 install ez_setup
+pip install unroll 
+pip3 install unroll 
+easy_install -U setuptools
+pip install unroll
+pip3 install unroll
 pip3 install -r requirements.txt
 easy_install -U setuptools
 pip3 install -r requirements.txt
+pip3 install cython
